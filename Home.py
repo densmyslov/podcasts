ed837858e0c207cabb5a2712e455bd027ad6c1e0
Denis Smyslov <dsmyslov@protonmail.com>
2024-07-01 14:46:06 +0000

    changed Home page

--- Home.py
+++ Home.py
@@ -11,11 +11,20 @@ AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 S3_BUCKET = os.environ.get('S3_BUCKET')
 FOLDER = os.environ.get('FOLDER')
+PASSWORD = os.environ.get('PASSWORD')
 
 s3_client = boto3.client('s3',
                          aws_access_key_id = AWS_ACCESS_KEY,
                         aws_secret_access_key = AWS_SECRET_ACCESS_KEY)
 
+if 'password' not in st.session_state:
+    st.session_state.password = ''
+st.session_state.password = st.text_input("password", 
+                        type='password')
+if st.session_state.password == PASSWORD:
+    st.success("Correct password")
+else:
+    st.error("Please enter password")
 
 # pages = [
 #         st.Page("./pages/youtube.py", title="Youtube link"),
